342e145e5b4d4317dd40f9e7d3328d83dca99675
7007singh <shw12021982@gmail.com>
2022-12-31 03:53:40 +0530

    Added: Start Flipping a Coin

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,1 +1,14 @@
 echo "Welcome to Flip Coin Combination Problem"
+
+read -p "Enter the number for flipping a coin " n
+
+for ((i=1; i<=$n; i++))
+do
+flip=$((RANDOM%2))
+if [ $flip -eq 1 ]
+then
+echo "heads"
+else
+echo "tails"
+fi
+done
